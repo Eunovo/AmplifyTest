6f0ae565afeb0965454bd899da75428b47802d83
Eunovo <eunovo9@gmail.com>
2020-05-30 20:47:44 +0100

    Change `class` to `className`

--- src/use-cases/view-task/components/ViewTaskList.jsx
+++ src/use-cases/view-task/components/ViewTaskList.jsx
@@ -22,7 +22,7 @@ export function ViewTaskList ({ ...rest }) {
       {tasks.isLoading ? (
         <div className='d-flex justify-content-center'>
           <div className='spinner-border text-primary' role='status'>
-            <span class='sr-only'>Loading...</span>
+            <span className='sr-only'>Loading...</span>
           </div>
         </div>
       ) : (
